02d6bf5adc1a40adae6b381d065a4b6ab0f3c772
Adriano Cardace <cardace6@gmail.com>
2017-08-03 18:20:09 +0200

    Update main.c

--- rowVersion/src/main.c
+++ rowVersion/src/main.c
@@ -16,77 +16,70 @@ unsigned clr = (unsigned)0x00000000;
 unsigned flag = (unsigned)0x00000001; 
  
 /* Returns 1 with probability p */
-int rand01( float p )
-{
-    return ((rand() / (float)RAND_MAX) < p);
+int rand01( float p ){
+	return ((rand() / (float)RAND_MAX) < p);
 }
 
-float timedifference_msec(struct timeval t0, struct timeval t1)
-{
-    return (t1.tv_sec - t0.tv_sec) * 1000.0f + (t1.tv_usec - t0.tv_usec) / 1000.0f;
+float timedifference_msec(struct timeval t0, struct timeval t1){
+	return (t1.tv_sec - t0.tv_sec) * 1000.0f + (t1.tv_usec - t0.tv_usec) / 1000.0f;
 }
 
-void dump_cur( const char* filename, char *grid)
-{
+void dump_cur( const char* filename, char *grid) {
 	int i, j;
-    FILE *out = fopen( filename, "w" );
-    if ( NULL == out ) {
-        printf("Cannot create file %s\n", filename );
-        abort();
-    }
-    fprintf(out, "P6\n");
-    fprintf(out, "%d %d\n", _Nelem, _Nelem);
-    fprintf(out, "255\n");
-    for (i=0; i<_Nelem; i++) {
-        for (j=0; j<_Nelem; j++) {
-            switch( grid[i*_Nelem +j] ) {
-            case 0:
-             fprintf(out, "%c%c%c", 255, 255, 255);
-                break;
-            case 1:
-               fprintf(out, "%c%c%c", 0, 0, 255);
-                break;
-            case 2:
-               fprintf(out, "%c%c%c", 255, 0, 0);
-                break;
-            default:
-                     printf("Error: unknown cell state %u\n", grid[i*_Nelem +j]);
-                abort();
-            }
-        
+	FILE *out = fopen( filename, "w" );
+	if ( NULL == out ) {
+		printf("Cannot create file %s\n", filename );
+	        abort();	
+	}
+	fprintf(out, "P6\n");
+	fprintf(out, "%d %d\n", _Nelem, _Nelem);
+	fprintf(out, "255\n");
+	for (i=0; i<_Nelem; i++) {
+		for (j=0; j<_Nelem; j++) {
+			switch( grid[i*_Nelem +j] ) {
+				case 0:
+					fprintf(out, "%c%c%c", 255, 255, 255);
+					break;
+				case 1:
+					fprintf(out, "%c%c%c", 0, 0, 255);
+					break;
+				case 2:
+					fprintf(out, "%c%c%c", 255, 0, 0);
+					break;
+				default:
+					printf("Error: unknown cell state %u\n", grid[i*_Nelem +j]);
+					abort();
+			}
 		}
-    }
-    fclose(out);
+	}
+	fclose(out);
 }
 
-void setup( float prob ) 
-{ 
-    int i, j; 
-    for ( i=0; i<_Nelem; i++ ) { 
-        for ( j=0; j<_Nelem; j++) { 
-            if ( rand01( prob) ) { 
-                Mailbox.grid[i*_Nelem+j] = ( rand01(0.5) ? LR : TB ); 
-            } else { 
-                Mailbox.grid[i*_Nelem+j] = EMPTY;
-            }
-        }
-    }
+void setup( float prob ) { 
+	int i, j; 
+	for ( i=0; i<_Nelem; i++ ) { 
+		for ( j=0; j<_Nelem; j++) { 
+			if ( rand01( prob) ) { 
+				Mailbox.grid[i*_Nelem+j] = ( rand01(0.5) ? LR : TB ); 
+			} else { 
+				Mailbox.grid[i*_Nelem+j] = EMPTY;
+			}
+		}
+	}
 }
 
 
-void bml_go(e_mem_t *pDRAM, int nsteps)
-{
+void bml_go(e_mem_t *pDRAM, int nsteps) {
 	float elapsed;
 	struct timeval stop, start;	
-    unsigned int addr;
-    size_t sz;
+	unsigned int addr;
+	size_t sz;
 	int i,j,k;
 	char buf[255];
 	unsigned done = 0;
 
 	gettimeofday(&start, NULL);      
-	for(k=1; k<nsteps; k++){
-		
+	for(k=1; k<nsteps; k++){	
 		while(1){
 			e_read(&dev, 0, 0, 0x7010, &done, sizeof(unsigned));
 			if (done == 1){
@@ -123,42 +116,40 @@ void bml_go(e_mem_t *pDRAM, int nsteps)
 #endif
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 	int i,j;
 	char buf[255];
-    e_mem_t DRAM;
-    unsigned int msize;
-    unsigned int addr;
+	e_mem_t DRAM;
+	unsigned int msize;
+	unsigned int addr;
 	int result;
-    size_t sz;
+	size_t sz;
 	float prob = 0.3;
-    int nsteps = 1024;
-    msize = 0x00400000;
-    if ( argc > 1 ) {
-        nsteps = atoi(argv[1]);
-    }
-    if ( argc > 2 ) {
-        prob = atof(argv[2]);
-    }
+	int nsteps = 1024;
+	msize = 0x00400000;
+	if ( argc > 1 ) {
+		nsteps = atoi(argv[1]);
+	}
+	if ( argc > 2 ) {
+		prob = atof(argv[2]);
+	}
    
 	//Initalize Epiphany device
 	e_init(NULL);                      
 	e_reset_system();                                      //reset Epiphany
 	e_get_platform_info(&platform);                          
 
-    if ( e_alloc(&DRAM, 0x00000000, msize) )
-    {
-        fprintf(stderr, "\nERROR: Can't allocate Epiphany DRAM!\n");
-        exit(1);
-    }
+	if ( e_alloc(&DRAM, 0x00000000, msize) ) {
+		fprintf(stderr, "\nERROR: Can't allocate Epiphany DRAM!\n");
+		exit(1);
+	}
 
 	if ( e_open(&dev, 0, 0, platform.rows, platform.cols) )
-//	if ( e_open(&dev, 0, 0, 2, 2) )
-    {
-        fprintf(stderr, "\nERROR: Can't establish connection to Epiphany device!\n");
-        exit(1);
-    }
+//	if ( e_open(&dev, 0, 0, 2, 2) ) 
+	{
+        	fprintf(stderr, "\nERROR: Can't establish connection to Epiphany device!\n");
+		exit(1);
+	}
 	//clear data and set parameters
 	e_write(&dev, 0, 0, 0x7000, &clr, sizeof(clr));
 	e_write(&dev, 0, 0, 0x7010, &clr, sizeof(clr));
@@ -171,39 +162,37 @@ int main(int argc, char *argv[])
 	setup(prob);
 
 	//write grid in shared memory	
-    addr = offsetof(shared_buf_t, grid);
-    sz = sizeof(Mailbox.grid);
-    fprintf(stderr, "Writing Mailbox.grid[%uB] to address %08x...\n", sz, addr);
-    e_write(&DRAM, 0, 0, addr, (void *) Mailbox.grid, sz);
+	addr = offsetof(shared_buf_t, grid);
+	sz = sizeof(Mailbox.grid);
+	fprintf(stderr, "Writing Mailbox.grid[%uB] to address %08x...\n", sz, addr);
+	e_write(&DRAM, 0, 0, addr, (void *) Mailbox.grid, sz);
 	
 	//print out first frame
 	snprintf(buf, 255, "out00000.ppm");
-    dump_cur(buf, Mailbox.grid);
+	dump_cur(buf, Mailbox.grid);
 
 	//Load program to cores
-    fprintf(stderr, "Loading program on Epiphany chip...\n");
+	fprintf(stderr, "Loading program on Epiphany chip...\n");
   	result = e_load_group("e_traffic.elf", &dev, 0, 0,platform.rows, platform.cols, E_FALSE);
-    //result = e_load_group("e_traffic.elf", &dev, 0, 0, 2, 2, E_FALSE);
-    if (result == E_ERR) {
-        fprintf(stderr, "Error loading Epiphany program.\n");
-        exit(1);
-    }
+	//result = e_load_group("e_traffic.elf", &dev, 0, 0, 2, 2, E_FALSE);
+	if (result == E_ERR) {
+        	fprintf(stderr, "Error loading Epiphany program.\n");
+        	exit(1);
+    	}
 	// start cores
 	e_start_group(&dev);
 
 	bml_go(&DRAM, nsteps);
 	
 	//Close down Epiphany device
-	if (e_close(&dev))
-    {
-        fprintf(stderr, "\nERROR: Can't close connection to Epiphany device!\n\n");
-        exit(1);
-    }
-    if (e_free(&DRAM))
-    {
-        fprintf(stderr, "\nERROR: Can't release Epiphany DRAM!\n\n");
-        exit(1);
-    }
+	if (e_close(&dev)) {
+		fprintf(stderr, "\nERROR: Can't close connection to Epiphany device!\n\n");
+		exit(1);
+	}
+	if (e_free(&DRAM)) {
+		fprintf(stderr, "\nERROR: Can't release Epiphany DRAM!\n\n");
+		exit(1);    
+	}
 	e_finalize();
 	return EXIT_SUCCESS;
 }

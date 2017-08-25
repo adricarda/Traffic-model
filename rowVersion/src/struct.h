27d2f316ad91d5913d7820c8c950ef4474eea5ea
Adriano Cardace <cardace6@gmail.com>
2017-08-25 16:32:31 +0200

    Update struct.h

--- rowVersion/src/struct.h
+++ rowVersion/src/struct.h
@@ -6,26 +6,25 @@
 #define _Nelem 128
 
 typedef struct {
-    unsigned coreID;
-    unsigned corenum;
-    unsigned row;
-    unsigned col;
-    unsigned row_next;
-    unsigned col_next;
-    unsigned row_previous;
-    unsigned col_previous;
-
+	unsigned coreID;
+	unsigned corenum;
+	unsigned row;
+	unsigned col;
+	unsigned row_next;
+	unsigned col_next;
+	unsigned row_previous;
+	unsigned col_previous;
 	char _grid[2][_Nelem* (_Nelem/_Ncores)];
 } core_t;
 
 typedef struct {
-    char grid[_Nelem*_Nelem];
+	char grid[_Nelem*_Nelem];
 	int nsteps;
 } shared_buf_t;
 
 typedef struct {
-    void  *pBase;   
-    char  *pGrid;   
+	void  *pBase;   
+	char  *pGrid;   
 	int *pNsteps;
 } shared_buf_ptr_t;
 
